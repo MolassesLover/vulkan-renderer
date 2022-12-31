438b9aae6c232800998b6911e40ac3cf0c301c3d
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-12-31 01:54:45 +0100

    Finished Python bindings

---
+++ src/bindings/python_example.py
@@ -0,0 +1,11 @@
+import TEMPORARYNAME
+from TEMPORARYNAME import *
+
+def main():
+    print("Running TEMPORARYNAME!")
+
+    app = VulkanApplication()
+    app.run()
+
+if __name__ == "__main__":
+    main()
